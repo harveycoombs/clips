4035ab0809adec8601493a71ea52ff0e25cb0530
harveycoombs <harveycoombs23@outlook.com>
2024-10-23 12:42:50 +0100

    key fixes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <>
             <main className="h-screen grid place-items-center">
-                <section className="w-64">
+                <section key="login" className="w-64">
                     <Link href="/" className="block text-lg text-center select-none font-bold duration-150 hover:opacity-65"><span className="text-indigo-500">clips</span>.harveycoombs.com</Link>
                     <div className="text-sm text-center select-none font-medium text-slate-400 mt-1 mb-8">Log In to Your Account</div>
                     <LoginForm />
