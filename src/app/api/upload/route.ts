ad827f2eb069bdd567dbe5a936e0a7cfb93591b0
harveycoombs <harveycoombs23@outlook.com>
2024-10-25 20:25:56 +0100

    buffer fix

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -24,7 +24,7 @@ export async function POST(request: Request): Promise<NextResponse> {
         await fs.mkdir(`./uploads/posts/${id}`);
 
         let buffer = Buffer.from(await file.arrayBuffer());
-        await fs.writeFile(`./uploads/posts/${id}/${file.name}`, buffer);
+        await fs.writeFile(`./uploads/posts/${id}/${file.name}`, new Uint8Array(buffer));
     } catch (ex: any) {
         return NextResponse.json({ error: ex.message }, { status: 500 });
     }
