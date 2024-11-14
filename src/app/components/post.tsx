08da78b4cba658b27bacf06a13cacff29ed4e7f8
harveycoombs <harveycoombs23@outlook.com>
2024-11-14 23:41:31 +0000

    post errors (2)

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -16,16 +16,16 @@ export default function Post(props: any) {
 
     let post = props.data;
 
-    let icon = useRef<HTMLDivElement>(null);
+    let hover = useRef<HTMLDivElement>(null);
 
     return (
         <Link href={`/posts/${post.postid}`} title={post.title} className="group bg-slate-50 aspect-square rounded-md overflow-hidden relative" key={post.postid}>
-            <video src={`/uploads/posts/${post.postid}`} className="w-full h-full object-cover" loop muted playsInline onMouseEnter={playVideo} onMouseLeave={stopVideo} onError={(e: any) => {
+            <video src={`/uploads/posts/${post.postid}`} className="w-full h-full object-cover z-10 relative" loop muted playsInline onMouseEnter={playVideo} onMouseLeave={stopVideo} onError={(e: any) => {
                 e.target.remove();
-                icon?.current?.remove();
+                hover?.current?.remove();
             }}></video>
             <FaCircleExclamation className="text-6xl absolute inset-0 m-auto text-slate-300/70" />
-            <div ref={icon} className="pointer-events-none absolute inset-0 bg-black text-white bg-opacity-50 place-items-center hidden group-hover:grid"><FaUpRightFromSquare className="text-4xl pointer-events-none" /></div>
+            <div ref={hover} className="pointer-events-none absolute inset-0 z-20 bg-black text-white bg-opacity-50 place-items-center hidden group-hover:grid"><FaUpRightFromSquare className="text-4xl pointer-events-none" /></div>
         </Link>
     );
 }
