de0eecf0c78e36facb4305e004fcf9afa13e135a
harveycoombs <harveycoombs23@outlook.com>
2024-11-24 21:02:08 +0000

    Player (2)

--- src/app/components/ui/player.tsx
+++ src/app/components/ui/player.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useRef } from "react";
 import { FaExpand, FaPause, FaPlay, FaVolumeHigh } from "react-icons/fa6";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface Properties {
     url: string;
@@ -9,11 +10,13 @@ interface Properties {
 
 export default function Player({ url, classes }: Properties) {
     let [playing, setPlaying] = useState<boolean>(false);
+    const [indicatorIsVisible, setIndicatorVisibility] = useState(true);
 
-    let seeking = false;
-
+    let video = useRef<HTMLVideoElement|null>(null);
     let seekbarContainer = useRef<HTMLDivElement|null>(null);
     let seekbar = useRef<HTMLDivElement|null>(null);
+
+    let seeking = false;
 
     function seek(e: any) {
         if (!seekbar?.current || !seekbarContainer?.current || !seeking) return;
@@ -23,10 +26,39 @@ export default function Player({ url, classes }: Properties) {
         seekbar.current.style.width = `${position}px`;
     }
 
+    function playVideo() {
+        if (!video.current) return;
+
+        if (playing) {
+            video.current.pause();
+        } else {
+            video.current.play();
+        }
+
+        setPlaying(!playing);
+        setIndicatorVisibility(true);
+    }
+
     return (
-        <div className="relative bg-slate-50 rounded-lg overflow-hidden aspect-video w-full text-white select-none">
+        <div className="relative bg-slate-50 rounded-lg overflow-hidden aspect-video w-full text-white select-none cursor-pointer" onClick={playVideo}>
+            <AnimatePresence>
+             <motion.div
+                    initial={{ scale: 1, opacity: 1 }}
+                    animate={{
+                    scale: 2,
+                    opacity: 0,
+                    }}
+                    exit={{ opacity: 0 }}
+                    transition={{
+                    duration: 2,
+                    ease: "easeOut",
+                    }}
+                    onAnimationComplete={() => setIndicatorVisibility(false)}
+                    className="text-2xl absolute inset-0 z-20 w-fit h-fit m-auto"
+                >{playing ? <FaPause /> : <FaPlay />}</motion.div>
+            </AnimatePresence>
             <div className="bg-gradient-to-t from-black/30 to-transparent pointer-events-none z-0 absolute h-[20%] bottom-0 left-0 right-0"></div>
-            <video src={url} className="w-full aspect-video"></video>
+            <video src={url} className="w-full aspect-video" ref={video}></video>
             <div className="absolute bottom-0 left-0 right-0 z-10 p-3 text-xl flex gap-3 items-center" onMouseMove={seek} onMouseUp={() => seeking = false} onMouseLeave={() => seeking = false}>
                 <button>{playing ? <FaPause /> : <FaPlay />}</button>
                 <button><FaVolumeHigh /></button>
