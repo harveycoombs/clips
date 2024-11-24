97e97eb8f228bbc6f4a296f03999e37083752c62
harveycoombs <harveycoombs23@outlook.com>
2024-11-24 21:08:14 +0000

    fixed pause/play button

--- src/app/components/ui/player.tsx
+++ src/app/components/ui/player.tsx
@@ -10,7 +10,8 @@ interface Properties {
 
 export default function Player({ url, classes }: Properties) {
     let [playing, setPlaying] = useState<boolean>(false);
-    const [indicatorIsVisible, setIndicatorVisibility] = useState(true);
+    let [indicatorIsVisible, setIndicatorVisibility] = useState(true);
+    let [indicatorKey, setIndicatorKey] = useState(0);
 
     let video = useRef<HTMLVideoElement|null>(null);
     let seekbarContainer = useRef<HTMLDivElement|null>(null);
@@ -37,26 +38,22 @@ export default function Player({ url, classes }: Properties) {
 
         setPlaying(!playing);
         setIndicatorVisibility(true);
+        setIndicatorKey(prev => prev + 1);
     }
 
     return (
         <div className="relative bg-slate-50 rounded-lg overflow-hidden aspect-video w-full text-white select-none cursor-pointer" onClick={playVideo}>
-            <AnimatePresence>
-             <motion.div
+            <motion.div
+                    key={indicatorKey}
                     initial={{ scale: 1, opacity: 1 }}
-                    animate={{
-                    scale: 2,
-                    opacity: 0,
-                    }}
-                    exit={{ opacity: 0 }}
+                    animate={{ scale: 2, opacity: 0 }}
                     transition={{
-                    duration: 2,
-                    ease: "easeOut",
+                        duration: 0.5,
+                        ease: "easeOut"
                     }}
                     onAnimationComplete={() => setIndicatorVisibility(false)}
-                    className="text-2xl absolute inset-0 z-20 w-fit h-fit m-auto"
+                    className="text-4xl absolute inset-0 z-20 w-fit h-fit m-auto"
                 >{playing ? <FaPause /> : <FaPlay />}</motion.div>
-            </AnimatePresence>
             <div className="bg-gradient-to-t from-black/30 to-transparent pointer-events-none z-0 absolute h-[20%] bottom-0 left-0 right-0"></div>
             <video src={url} className="w-full aspect-video" ref={video}></video>
             <div className="absolute bottom-0 left-0 right-0 z-10 p-3 text-xl flex gap-3 items-center" onMouseMove={seek} onMouseUp={() => seeking = false} onMouseLeave={() => seeking = false}>
